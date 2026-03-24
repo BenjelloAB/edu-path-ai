825d10028f358af722071c785b1b190e74adae75
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2026-03-24 21:50:57 +0000

    switching to azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -11,160 +11,249 @@ terraform {
   }
 }
 
-# 1. PROVIDER: The API Connector
-provider "aws" {
-  region = "eu-west-3" 
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
-# 2. VPC: The Isolated Network
-resource "aws_vpc" "agile_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true # Essential for K8s internal DNS
-  enable_dns_hostnames = true 
-  tags = { Name = "agile-project-vpc" }
+variable "subscription_id" { sensitive = true }
+variable "client_id"       { sensitive = true }
+variable "client_secret"   { sensitive = true }
+variable "tenant_id"       { sensitive = true }
+variable "location"        { default = "West Europe" }
+
+resource "azurerm_resource_group" "main" {
+  name     = "agile-rg"
+  location = var.location
 }
 
-# 3. INTERNET GATEWAY: The "Front Door" 
-resource "aws_internet_gateway" "main_gw" {
-  vpc_id = aws_vpc.agile_vpc.id
-  tags   = { Name = "agile-gw" }
+resource "azurerm_virtual_network" "main" {
+  name                = "agile-vnet"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["10.0.0.0/16"]
 }
 
-# 4. SUBNET: The "Floor"
-resource "aws_subnet" "main_subnet" {
-  vpc_id                  = aws_vpc.agile_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true # Gives EC2s a Public IP address
-  tags                    = { Name = "agile-public-subnet" }
+resource "azurerm_subnet" "main" {
+  name                 = "agile-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-# 5. ROUTE TABLE: The "Map" to the Internet
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.agile_vpc.id
+resource "azurerm_network_security_group" "main" {
+  name                = "agile-nsg"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
 
-  route {
-    cidr_block = "0.0.0.0/0" # Send all outgoing traffic
-    gateway_id = aws_internet_gateway.main_gw.id # to the Internet Gateway
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "K3sAPI"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "6443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "FlannelVXLAN"
+    priority                   = 140
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "8472"
+    source_address_prefix      = "10.0.0.0/16"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "KubeletMetrics"
+    priority                   = 150
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10250"
+    source_address_prefix      = "10.0.0.0/16"
+    destination_address_prefix = "*"
   }
 }
 
-# 6. ROUTE TABLE ASSOCIATION: Linking the Floor to the Map
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+resource "azurerm_public_ip" "master" {
+  name                = "master-pip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
 }
 
-# 7. SECURITY GROUP: The "Firewall"
-resource "aws_security_group" "k3s_sg" {
-  name   = "k3s-security-group"
-  vpc_id = aws_vpc.agile_vpc.id
+resource "azurerm_public_ip" "worker" {
+  name                = "worker-pip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
 
-  # Inbound: SSH for Ansible and manual debugging
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
+resource "azurerm_network_interface" "master" {
+  name                = "master-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
 
-  # HTTP - user traffic, Nginx Ingress listens here
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTPS - for TLS
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  # K3s API - for kubectly and GHA deploys
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # K3s inter-node communication (Flannel VXLAN)
-  ingress {
-    from_port   = 8472
-    to_port     = 8472
-    protocol    = "udp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  # K3s node metrics
-  ingress {
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  # Outbound: Allow EVERYTHING
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  ip_configuration {
+    name                          = "master-ipconfig"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.master.id
   }
 }
 
-# 8. EC2 INSTANCES: The "Compute"
-# NOTE: You MUST have a key pair created in AWS Console named "my-key"
-resource "aws_instance" "master" {
-  ami                    = "ami-00ac45f33470c3095"  # to change
-  instance_type          = "t3.micro" #
-  subnet_id              = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
-  key_name               = "my-key" # <---  AWS SSH KEY NAME HERE toc.
-  
-  tags = { Name = "k3s-master" }
+resource "azurerm_network_interface" "worker" {
+  name                = "worker-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "worker-ipconfig"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.worker.id
+  }
 }
 
-resource "aws_instance" "worker" {
-  ami                    = "ami-00ac45f33470c3095" # toc.
-  instance_type          = "t3.micro" # toc.
-  subnet_id              = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
-  key_name               = "my-key" # <---  AWS SSH KEY NAME HERE
-
-  tags = { Name = "k3s-worker" }
+resource "azurerm_network_interface_security_group_association" "master" {
+  network_interface_id      = azurerm_network_interface.master.id
+  network_security_group_id = azurerm_network_security_group.main.id
 }
 
-# Outputs - copy these into ansible/inventory.ini after terraform apply
-output "master_public_ip" {
-  value = aws_instance.master.public_ip
+resource "azurerm_network_interface_security_group_association" "worker" {
+  network_interface_id      = azurerm_network_interface.worker.id
+  network_security_group_id = azurerm_network_security_group.main.id
 }
 
-output "worker_public_ip" {
-  value = aws_instance.worker.public_ip
+resource "azurerm_linux_virtual_machine" "master" {
+  name                = "k3s-master"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1ms"
+  admin_username      = "ubuntu"
+
+  network_interface_ids = [
+    azurerm_network_interface.master.id
+  ]
+
+  admin_ssh_key {
+    username   = "ubuntu"
+    public_key = file("~/.ssh/my-key.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
 
-## should i add this ? where will it be used ? 
-output "master_private_ip" {
-  value = aws_instance.master.private_ip
+resource "azurerm_linux_virtual_machine" "worker" {
+  name                = "k3s-worker"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1ms"
+  admin_username      = "ubuntu"
+
+  network_interface_ids = [
+    azurerm_network_interface.worker.id
+  ]
+
+  admin_ssh_key {
+    username   = "ubuntu"
+    public_key = file("~/.ssh/my-key.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
 
 resource "local_file" "ansible_inventory" {
   content = <<EOT
 [master]
-master_node ansible_host=${aws_instance.master.public_ip}
+master_node ansible_host=${azurerm_public_ip.master.ip_address}
 
 [worker]
-worker_node ansible_host=${aws_instance.worker.public_ip}
+worker_node ansible_host=${azurerm_public_ip.worker.ip_address}
 
 [all:vars]
 ansible_user=ubuntu
-ansible_ssh_private_key_file=~/.ssh/my-key.pem
+ansible_ssh_private_key_file=~/.ssh/my-key
 ansible_python_interpreter=/usr/bin/python3
 EOT
   filename = "../ansible/inventory.ini"
+}
+
+output "master_public_ip" {
+  value = azurerm_public_ip.master.ip_address
+}
+
+output "worker_public_ip" {
+  value = azurerm_public_ip.worker.ip_address
+}
+
+output "master_private_ip" {
+  value = azurerm_network_interface.master.private_ip_address
 }
