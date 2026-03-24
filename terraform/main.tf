7130241f1233cc4a77d7cfb58483f4b6c70ca9e8
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2026-03-24 23:04:01 +0000

    fixing region issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ variable "subscription_id" { sensitive = true }
 variable "client_id"       { sensitive = true }
 variable "client_secret"   { sensitive = true }
 variable "tenant_id"       { sensitive = true }
-variable "location"        { default = "spaincentral" }
+variable "location"        { default = "germanywestcentral" }
 
 resource "azurerm_resource_group" "main" {
   name     = "agile-rg"
